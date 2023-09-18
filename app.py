a6c6dd42db8a872d8b0b1501f9c9ba0b34e3d675
rockwillck <38663064+rockwillck@users.noreply.github.com>
2023-09-17 21:23:06 -0500

    weird bug with stopping recording: hacky solution for now

--- app.py
+++ app.py
@@ -9,6 +9,11 @@ mousePosition = [pyautogui.size()[0]/2, pyautogui.size()[1]/2]
 dimensions = [512, 256]
 images = []
 easedMousePostion = mousePosition
+smoothing = 0.1
+
+def setSmoothing(factor):
+    global smoothing
+    smoothing = factor
 
 def setDimensions(x, y):
     global dimensions
@@ -18,27 +23,38 @@ def takeSS():
     global mousePosition
     global easedMousePostion
     global images
+
     supposed = pyautogui.position()
     mousePosition = [
         clamp(supposed[0], pyautogui.size()[0] - dimensions[0], dimensions[0]),
         clamp(supposed[1], pyautogui.size()[1] - dimensions[1], dimensions[1])
     ]
-    easedMousePostion = [easedMousePostion[0] + (mousePosition[0] - easedMousePostion[0])*0.9, easedMousePostion[1] + (mousePosition[1] - easedMousePostion[1])*0.9]
+    easedMousePostion = [easedMousePostion[0] + (mousePosition[0] - easedMousePostion[0])*smoothing, easedMousePostion[1] + (mousePosition[1] - easedMousePostion[1])*smoothing]
 
     im2 = ImageGrab.grab(bbox =(round(easedMousePostion[0]) - dimensions[0], round(easedMousePostion[1]) - dimensions[1], round(easedMousePostion[0]) + dimensions[0], round(easedMousePostion[1]) + dimensions[1]))
     images.append(im2)
 
 recording = True
 def start():
+    global easedMousePostion
+    global images
+
+    recording = True
+
+    images = []
+
+    easedMousePostion = [pyautogui.position()[0], pyautogui.position()[1]]
     while recording:
         takeSS()
 
+stoppedImages = []
 def stop():
     global recording
+    global stoppedImages
     recording = False
-    images = []
+    stoppedImages = images[0:]
 
 def save(fileName, fps):
-    frame_one = images[0]
-    frame_one.save(fileName, format="GIF", append_images=images,
-                save_all=True, loop=0, duration=round(len(images)*fps))
+    frame_one = stoppedImages[0]
+    frame_one.save(fileName, format="GIF", append_images=stoppedImages,
+                save_all=True, loop=0, duration=round(len(stoppedImages)*fps))
--- swoosh.py
+++ swoosh.py
@@ -9,9 +9,12 @@ def save_to_file():
     if filename:
         app.save(filename, float(entry_num3.get()))
 
+calculation_thread = 0
 def start_calculation():
+    global calculation_thread
     try:
         app.setDimensions(round(int(entry_num1.get())/2), round(int(entry_num2.get())/2))
+        app.setSmoothing((101 - int(entry_num4.get()))/101)
         
         # Create a thread to run app.start()
         calculation_thread = threading.Thread(target=app.start)
@@ -22,9 +25,9 @@ def start_calculation():
         tk.messagebox.showerror(title="Error", message="Invalid Input")
 
 def stop_calculation():
+    app.stop()
     start_button.config(state=tk.NORMAL)
     stop_button.config(state=tk.DISABLED)
-    app.stop()
 
 # Create the main window
 root = tk.Tk()
@@ -37,10 +40,12 @@ root.resizable(False, False)
 label_num1 = tk.Label(root, text="Width:")
 label_num2 = tk.Label(root, text="Height:")
 label_num3 = tk.Label(root, text="FPS Multiplier:")
+label_num4 = tk.Label(root, text="Smoothing:")
 entry_num1 = tk.Entry(root)
 entry_num2 = tk.Entry(root)
 entry_num3 = tk.Entry(root)
 entry_num3.insert(0, "1.5")
+entry_num4 = tk.Scale(root, from_=0, to=100, orient=tk.HORIZONTAL)
 
 # Create and configure buttons
 start_button = tk.Button(root, text="Start Recording", command=start_calculation)
@@ -56,11 +61,13 @@ label_num2.grid(row=1, column=0, padx=10, pady=10)
 entry_num2.grid(row=1, column=1, padx=10, pady=10)
 label_num3.grid(row=2, column=0, padx=10, pady=10)
 entry_num3.grid(row=2, column=1, padx=10, pady=10)
+label_num4.grid(row=3, column=0, padx=10, pady=10)
+entry_num4.grid(row=3, column=1, padx=10, pady=10)
 
-start_button.grid(row=3, column=0, padx=10, pady=10)
-stop_button.grid(row=3, column=1, padx=10, pady=10)
+start_button.grid(row=4, column=0, padx=10, pady=10)
+stop_button.grid(row=4, column=1, padx=10, pady=10)
 
-browse_button.grid(row=4, column=0, columnspan=2, padx=10, pady=10)
+browse_button.grid(row=5, column=0, columnspan=2, padx=10, pady=10)
 
 # Start the main loop
 root.mainloop()
